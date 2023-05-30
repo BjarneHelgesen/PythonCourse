438eeb6a11d62a337ed7b5778eccbff789cedd43
Bjarne Helgesen <bjarne@helgesen.biz>
2023-05-30 18:30:42 +0200

    Added keyboard input, collision function

--- Lesson6b.py
+++ Lesson6b.py
@@ -27,11 +27,24 @@ class Element:
         return self.top + self.height
     
     def update(self):
-        self.left += 1
+        ...
 
     def blit(self):
         rect = pygame.Rect((self.left, self.top), (self.width, self.height))
         screen.blit(self.surface, rect)
+
+def overlap(element1, element2):
+     if element1.bottom < element2.top or element2.bottom < element1.top:
+         return False
+     if element1.right < element2.left or element2.right < element1.left:
+         return False
+     return True 
+
+def movement(negative_key, postive_key, value):  
+    """Return -value if negative_key is pressed, value if positive_key is pressed, 0 if none of of them or both of them are pressed
+    Keys could be pygame.K_LEFT, pygame.K_RIGHT, etc"""
+    keys_pressed = pygame.key.get_pressed()
+    return -keys_pressed[negative_key]*value + keys_pressed[postive_key]*value
 
 screen = pygame.display.set_mode((600, 400))
 clock = pygame.time.Clock()
@@ -47,6 +60,7 @@ while True:
 
     clock.tick(60)
     egg.update()
+    egg.left += movement(pygame.K_LEFT, pygame.K_RIGHT, 1)
 
     background.blit()
     egg.blit()
